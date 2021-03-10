6db4f83835d7c94e3d616991ce6a8104e697278b
upship-kim <tkdqook@gmail.com>
2021-03-10 22:03:27 +0900

    react-virtualized 테스트

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized'
 import './TodoList.scss';
 
 
 const TodoList = ({ todos, onRemove, onToggle }) => { //todos 와 onRemove를 받겠다. 
-    return (
-        <div className="TodoList">
-            {todos.map(todo => (    //여기서 todo는 지정한것? 
-                <TodoListItem todo={todo} key={todo.id} 
-                onRemove={onRemove} onToggle={onToggle}/>
-            ))}
-        </div>
-    )
-}
-
-export default TodoList
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem 
+                    todo={todo} 
+                    key={todo.id} 
+                    onRemove={onRemove} 
+                    onToggle={onToggle}
+                    style={style}
+                    />
+            );
+        },
+        [onRemove, onToggle, todos],
+    );
+    return( //list 전체의 모습을 return
+        <List
+            className="TodoList"
+            width={512} //전체크기
+            height={513}    //전체 높이
+            rowCount={todos.length} //항목 개수
+            rowHeight={57}
+            rowRenderer={rowRenderer} // 리스트 항목을 렌더링할때 쓰는 함수
+            list={todos} // 배열
+            style={{ outline:'none'}}   //List에 기본 적용되는 outline 스타일 제거
+        />
+        
+    );   
+};
+export default React.memo(TodoList)
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,9 +7,10 @@ import{
 import './TodoListItem.scss';
 import cname from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;     //todo에 포함되어있는 속성들 
     return (
+    <div className="TodoListItem-virtualized" style={style}>
         <div className="TodoListItem">
             {/* classnames library 사용  */}
             <div className={cname("checkbox", {checked})} onClick={()=>onToggle(id)}> 
@@ -20,7 +21,9 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
                 <MdRemoveCircleOutline/>
             </div>
         </div>
+    </div>
+
     )
 }
 
-export default React.memo(TodoListItem);
+export default React.memo(TodoListItem);    //React.memo = 리렌더링을 방지해주는 함수, 즉 TodoListItem의 todo, onRemove, onToggle 이 변하지 않으면 렌더링 하지 않음 
